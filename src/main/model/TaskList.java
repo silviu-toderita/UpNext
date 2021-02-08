2eb2b9f71649e0014cb78c0c1d533752754c7a0f
Silviu Toderita <silviu.toderita@gmail.com>
2021-02-08 15:04:00 -0800

    Completed all tests for TaskList class

--- src/main/model/TaskList.java
+++ src/main/model/TaskList.java
@@ -19,8 +19,21 @@ public class TaskList {
         // Stub
     }
 
+    // REQUIRES: id must be a valid index in the task list
+    // MODIFIES: this
+    // EFFECTS: Removes task with given id from task list
+    public void complete(int id) {
+        // Stub
+    }
+
     // EFFECTS: Returns the size of the task list
     public int size() {
+        return 0; // Stub
+    }
+
+    // REQUIRES: name must be >= 1 character in size
+    // EFFECTS: Returns the id of the first task with matching name, or -1 if it's not in list
+    public int find(String name) {
         return 0; // Stub
     }
 
@@ -30,18 +43,7 @@ public class TaskList {
         return null; // Stub
     }
 
-    // REQUIRES: name must be >= 1 character in size
-    // EFFECTS: Returns the id of a task with matching name, or -1 if it can't be found
-    public int find(String name) {
-        return 0; // Stub
-    }
 
-    // REQUIRES: id must be a valid index in the task list
-    // MODIFIES: this
-    // EFFECTS: Removes task with given id from task list
-    public void complete(int id) {
-        // Stub
-    }
 
 
 }
--- src/test/model/TaskListTest.java
+++ src/test/model/TaskListTest.java
@@ -1,16 +1,186 @@
 package model;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class TaskListTest {
+public class TaskListTest {
 
     TaskList taskList;
+    Task testTaskA;
+    Task testTaskB;
+    Task testTaskC;
 
     @Before
     public void setUp(){
         taskList = new TaskList();
+        Date testDate = new Date(2020,03,23);
+        testTaskA = new Task("Groceries", testDate, 3);
+        testTaskB = new Task("Volunteering", testDate, 2);
+        testTaskC = new Task("CPSC 210 Project", testDate, 5);
+
+        assertEquals(0, taskList.size());
+    }
+
+    @Test
+    public void testAddTaskEmptyList() {
+        taskList.add(testTaskA);
+        assertEquals(1, taskList.size());
+        assertEquals(testTaskA, taskList.get(0));
+    }
+
+    @Test
+    public void testAddMultipleTasks() {
+        taskList.add(testTaskA);
+        assertEquals(1, taskList.size());
+        assertEquals(testTaskA, taskList.get(0));
+
+        taskList.add(testTaskB);
+        assertEquals(2, taskList.size());
+        assertEquals(testTaskA, taskList.get(0));
+        assertEquals(testTaskB, taskList.get(1));
+
+        taskList.add(testTaskC);
+        assertEquals(3, taskList.size());
+        assertEquals(testTaskA, taskList.get(0));
+        assertEquals(testTaskB, taskList.get(1));
+        assertEquals(testTaskC, taskList.get(2));
+    }
+
+    @Test
+    public void testCompleteOnlyTask(){
+        taskList.add(testTaskB);
+        assertEquals(1, taskList.size());
+
+        taskList.complete(0);
+        assertEquals(0, taskList.size());
+    }
+
+    @Test
+    public void testCompleteOneTaskOfMany(){
+        taskList.add(testTaskA);
+        taskList.add(testTaskB);
+        assertEquals(2, taskList.size());
+        assertEquals(testTaskA, taskList.get(0));
+
+        taskList.complete(0);
+        assertEquals(1, taskList.size());
+        assertEquals(testTaskB, taskList.get(0));
+    }
+
+    @Test
+    public void testCompleteTwoTasksOfMany(){
+        taskList.add(testTaskA);
+        taskList.add(testTaskB);
+        taskList.add(testTaskC);
+        assertEquals(3, taskList.size());
+        assertEquals(testTaskA, taskList.get(0));
+
+        taskList.complete(0);
+        taskList.complete(1);
+        assertEquals(1, taskList.size());
+        assertEquals(testTaskB, taskList.get(0));
+    }
+
+    @Test
+    public void testCompleteManyTasks(){
+        taskList.add(testTaskA);
+        taskList.add(testTaskB);
+        taskList.add(testTaskC);
+        assertEquals(3, taskList.size());
+
+        taskList.complete(2);
+        taskList.complete(0);
+        taskList.complete(1);
+        assertEquals(0, taskList.size());
+        assertEquals(testTaskB, taskList.get(0));
+    }
+
+    @Test
+    public void testAddTaskAndCompleteTwice(){
+        taskList.add(testTaskC);
+        assertEquals(1, taskList.size());
+
+        taskList.complete(0);
+        assertEquals(0,taskList.size());
+
+        taskList.add(testTaskB);
+        assertEquals(1, taskList.size());
+        assertEquals(testTaskB, taskList.get(0));
+
+        taskList.complete(0);
+        assertEquals(0,taskList.size());
+    }
+
+    @Test
+    public void testAddTasksAndCompleteTasksMultiple(){
+        taskList.add(testTaskC);
+        taskList.add(testTaskA);
+        assertEquals(2, taskList.size());
+
+        taskList.complete(1);
+        assertEquals(1,taskList.size());
+        assertEquals(testTaskC, taskList.get(0));
+
+        taskList.add(testTaskB);
+        taskList.add(testTaskA);
+        assertEquals(3, taskList.size());
+        assertEquals(testTaskB, taskList.get(1));
+
+        taskList.complete(2);
+        taskList.complete(0);
+        assertEquals(1,taskList.size());
+        assertEquals(testTaskB, taskList.get(0));
+    }
+
+    @Test
+    public void testFindTaskEmptyList(){
+        assertEquals(-1, taskList.find("Groceries"));
+    }
+
+    @Test
+    public void testFindOnlyTask(){
+        taskList.add(testTaskA);
+        assertEquals(0, taskList.find("Groceries"));
+    }
+
+    @Test
+    public void testCantFindOnlyTask(){
+        taskList.add(testTaskA);
+        assertEquals(-1, taskList.find("Volunteering"));
+    }
+
+    @Test
+    public void testFindTaskAmongMultiple(){
+        taskList.add(testTaskA);
+        taskList.add(testTaskC);
+        taskList.add(testTaskB);
+        assertEquals(1, taskList.find("Volunteering"));
+    }
+
+    @Test
+    public void testCantFindTaskAmongMultiple(){
+        taskList.add(testTaskA);
+        taskList.add(testTaskB);
+        taskList.add(testTaskC);
+        assertEquals(-1, taskList.find("ENGL 301 Homework"));
+    }
+
+    @Test
+    public void testFindTaskAndComplete(){
+        taskList.add(testTaskA);
+        taskList.add(testTaskB);
+        taskList.add(testTaskC);
+        assertEquals(3, taskList.size());
+
+        int id = taskList.find("Volunteering");
+        assertEquals(1, id);
+        assertEquals(testTaskB, taskList.get(id));
+        taskList.complete(id);
+        assertEquals(2, taskList.size());
     }
 
 }
